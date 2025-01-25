b3e584bae45fbb29472ce471553a34e96272678c
Mitchell R <github@mrincworld.com>
2025-01-25 04:45:03 +0200

    Quick Tenant Setup - Disabling dns check

--- app/betterpbx/resources/functions/quick_setup.php
+++ app/betterpbx/resources/functions/quick_setup.php
@@ -344,26 +344,26 @@ function quick_setup($data){
     $data['stage'] = 'domain';
   }
 
-  if ($data['stage'] == 'domain') {
-    $domain = $data['domain'];
-    $ip = gethostbyname($domain);
-    $output = shell_exec('ip a');
-    $lines = explode("\n", $output);
-    $ipsOnServer = [];
+  // if ($data['stage'] == 'domain') {
+  //   $domain = $data['domain'];
+  //   $ip = gethostbyname($domain);
+  //   $output = shell_exec('ip a');
+  //   $lines = explode("\n", $output);
+  //   $ipsOnServer = [];
 
-    foreach ($lines as $line) {
-      if (preg_match('/inet\s+([0-9.]+)/', $line, $matches)) {
-        $ipsOnServer[] = $matches[1];
-      }
-    }
+  //   foreach ($lines as $line) {
+  //     if (preg_match('/inet\s+([0-9.]+)/', $line, $matches)) {
+  //       $ipsOnServer[] = $matches[1];
+  //     }
+  //   }
 
-    if (!in_array($ip, $ipsOnServer)) {
-      $message = "The domain does not point to the server (current points to " . $ip . "). \nCheck the domain is pointing to one of the following IPs: \n" . implode("\n", $ipsOnServer);
-      message::add($message, 'negative', 5000);
-      return false;
-    }
-    unset($ipsOnServer);
-  }
+  //   if (!in_array($ip, $ipsOnServer)) {
+  //     $message = "The domain does not point to the server (current points to " . $ip . "). \nCheck the domain is pointing to one of the following IPs: \n" . implode("\n", $ipsOnServer);
+  //     message::add($message, 'negative', 5000);
+  //     return false;
+  //   }
+  //   unset($ipsOnServer);
+  // }
 
   $database = database::new();
   
